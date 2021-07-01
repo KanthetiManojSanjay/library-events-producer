8f8c7aea0770b0551d0b3ad8e2fed3833b799d3e
kansanja <sanjay.kantheti@e.lloydsbanking.com>
2021-07-01 13:22:07 +0530

    Initial commit

---
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -0,0 +1,52 @@
+package com.learnkafka.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
+import com.learnkafka.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+@RestController
+@Slf4j
+public class LibraryEventsController {
+
+    @Autowired
+    LibraryEventProducer libraryEventProducer;
+
+    @PostMapping("/v1/libraryEvent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+
+        //invoke kafka producer
+        log.info("before sendLibraryEvent");
+        //libraryEventProducer.sendLibraryEvent(libraryEvent);
+        //SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
+        libraryEvent.setLibraryEventType(LibraryEventType.NEW);
+        libraryEventProducer.sendLibraryEvent_Approach2(libraryEvent);
+        //log.info("SendResult is {}", sendResult.toString());
+        log.info("after sendLibraryEvent");
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+
+    @PutMapping("/v1/libraryEvent")
+    public ResponseEntity<?> putLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+
+        if(libraryEvent.getLibraryEventId()==null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass libraryEventId");
+        }
+        libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
+        libraryEventProducer.sendLibraryEvent_Approach2(libraryEvent);
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
+    }
+}
+
